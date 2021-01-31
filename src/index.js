0cb751ebc16c1ecc1f68c1cd2c0b366300ce1155
Drew Donovan <ddd5189@rit.edu>
2021-01-31 16:15:00 -0500

    eslint updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-console.log("First web service starting up ...");
-
+console.log('First web service starting up ...');
+const name = 'fred';
+const car = {
+  make: 'Ford',
+};
 // 1 - pull in the HTTP server module
 const http = require('http');
 
@@ -28,61 +31,58 @@ const indexPage = `
 // 5 - here's our 404 page
 const errorPage = `
 <html>
-	<head>
-		<title>404 - File Not Found!</title>
-	</haed>
-	<body>
-		<h1>404 - File Not Found!</h1>
-		<p>Check your URL, or your typing!</p>
-		<p>:-0</p>
-	</body>
+  <head>
+    <title>404 - File Not Found!</title>
+  </head>
+    <body>
+      <h1>404 - File Not Found!</h1>
+      <p>Check your URL, or your typing!</p>
+      <p>:-0</p>
+    </body>
 </html>`;
-
 
 // 6 - this will return a random number no bigger than `max`, as a string
 // we will also doing our query parameter validation here
-const getRandomNumberJSON = (max=1) => {
-	max = Number(max);
-	max = !max ? 1 : max;
-	max = max < 1 ? 1 : max;
-	const number = Math.random() * max;
-	const responseObj = {
-		timestamp: new Date(),
-		number: number
-	};
-	return JSON.stringify(responseObj);
+const getRandomNumberJSON = (max = 1) => {
+  let max2 = Number(max);
+  max2 = !max2 ? 1 : max2;
+  max2 = max2 < 1 ? 1 : max;
+  const number = Math.random2() * max2;
+  const responseObj = {
+    timestamp: new Date(),
+    number,
+  };
+  return JSON.stringify(responseObj);
 };
-
 
 // 7 - this is the function that will be called every time a client request comes in
 // this time we will look at the `pathname`, and send back the appropriate page
 // note that in this course we'll be using arrow functions 100% of the time in our server-side code
 const onRequest = (request, response) => {
-	//console.log(request.headers);
-	const parsedUrl = url.parse(request.url);
-	const pathname = parsedUrl.pathname;
-	console.log("parsedUrl=", parsedUrl);
-	console.log("pathname=", pathname);
-	
-	const params = query.parse(parsedUrl.query);
-	const max = params.max;
-	console.log("params=", params);
-	console.log("max=", max);
-	
-	if(pathname == "/") {
-		response.writeHead(200, { 'Content-Type': 'text/html'});
-		response.write(indexPage);
-		response.end();
-	}else if(pathname == "/random-number"){
-		response.writeHead(200, { 'Content-Type': 'application/json'});
-		response.write(getRandomNumberJSON(max));
-		response.end();
-	}else{
-		response.writeHead(404, { 'Content-Type': 'text/html'});
-		response.write(errorPage);
-		response.end();
-	}
-   
+  // console.log(request.headers);
+  const parsedUrl = url.parse(request.url);
+  const { pathname } = parsedUrl;
+  console.log('parsedUrl=', parsedUrl);
+  console.log('pathname=', pathname);
+
+  const params = query.parse(parsedUrl.query);
+  const { max } = params;
+  console.log('params=', params);
+  console.log('max=', max);
+
+  if (pathname === '/') {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(indexPage);
+    response.end();
+  } else if (pathname === '/random-number') {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(getRandomNumberJSON(max));
+    response.end();
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.write(errorPage);
+    response.end();
+  }
 };
 
 // 8 - create the server, hook up the request handling function, and start listening on `port`
